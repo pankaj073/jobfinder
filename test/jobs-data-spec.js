c9af9613cd8873c937d9a79efa804a1b84540782
Pankaj Sharma <pankaj073@gmail.com>
2016-08-08 23:23:21 +0530

    use Promise.resolve

--- test/jobs-data-spec.js
+++ test/jobs-data-spec.js
@@ -1,7 +1,9 @@
 var expect = require('chai').expect;
 var mongoose = require('mongoose');
+
 var jobModel = require('../models/job.js')
 var Promise = require('bluebird');
+mongoose.Promise = Promise;
 // turn it into a promise so we dont need call back
 function resetJobs() {
     return new Promise(function(resolve, reject) {
@@ -11,16 +13,18 @@ function resetJobs() {
 
 var connectDB = Promise.promisify(mongoose.connect, {context: mongoose});
 
+function findJobs(query) {
+    return Promise.resolve(mongoose.model('Job').find(query).exec());
+}
 describe("get jobs", function() {
     it("should never be empty since jobs are seeded", function(done) {
         connectDB('mongodb://localhost/jobfinder')
         .then(resetJobs)
         .then(jobModel.seedJobs)
-        .then(function() {
-            mongoose.model('Job').find({}).exec(function(error, jobsList) {
-                expect(jobsList.length).to.be.at.least(1);
-                done();
-            });
+        .then(findJobs)
+        .then(function(jobsList) {
+            expect(jobsList.length).to.be.at.least(1);
+            done();
         });
     });
 });

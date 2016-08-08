d32edffed052098abbde12647707ed76be1d980e
Pankaj Sharma <pankaj073@gmail.com>
2016-08-08 21:46:04 +0530

    mongo db done

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var jobModel = require('./models/job');
 var app = express();
 
 app.set('views', __dirname);
 app.set('view engine', 'pug');
 
 app.use(express.static(__dirname + '/public'))
+
+app.get('/api/jobs', function(req, res) {
+    mongoose.model('Job').find({}).exec(function(error, collection) {
+        res.send(collection);
+    })
+});
+
 app.get('*', function(req, res) {
     res.render('index');
 });
 
+mongoose.connect('mongodb://localhost/jobfinder');
+var con = mongoose.connection;
+
+con.once('open', function() {
+    console.log('connected to mongodb successfully');
+    jobModel.seedJobs();
+});
 app.listen(3000);

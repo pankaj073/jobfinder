a224458b1da03fa9a193232c0edbed18565ee32d
Pankaj Sharma <pankaj073@gmail.com>
2016-08-08 20:38:18 +0530

    root

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.set('views', __dirname);
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/public'))
+app.get('*', function(req, res) {
+    res.render('index');
+});
+
+app.listen(3000);
